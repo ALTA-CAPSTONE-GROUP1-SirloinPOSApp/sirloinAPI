f0e5dd94d8064eb0a29f11a91fa0dd0bbfaa6c3c
Muhamad Fauzan Putra <business.mfauzanptra@gmail.com>
2023-02-02 22:18:31 +0700

    create transaction model

---
+++ features/transaction/data/model.go
@@ -0,0 +1,53 @@
+package data
+
+import (
+	product "sirloinapi/features/product/data"
+	"sirloinapi/features/transaction"
+	user "sirloinapi/features/user/data"
+	"time"
+)
+
+type Transaction struct {
+	ID            uint `gorm:"PRIMARY_KEY;AUTO_INCREMENT;NOT NULL"`
+	UserId        uint
+	CustomerId    uint
+	TotalPrice    float64
+	Discount      float64
+	TotalBill     float64
+	CreatedAt     time.Time
+	OrderStatus   string
+	ProductStatus string
+	InvoiceNumber string
+	InvoiceUrl    string
+	PaymentUrl    string
+
+	User user.User `gorm:"foreignkey:UserId;association_foreignkey:ID"`
+}
+
+type TransactionProduct struct {
+	TransactionId uint
+	ProductId     uint
+	Quantity      int
+	Price         float64
+	TotalPrice    float64
+
+	Transaction Transaction     `gorm:"foreignkey:TransactionId;association_foreignkey:ID"`
+	Product     product.Product `gorm:"foreignkey:ProductId;association_foreignkey:ID"`
+}
+
+func DataToCoreT(data Transaction) transaction.Core {
+	return transaction.Core{
+		ID:            data.ID,
+		UserId:        data.UserId,
+		CustomerId:    data.CustomerId,
+		TotalPrice:    data.TotalPrice,
+		Discount:      data.Discount,
+		TotalBill:     data.TotalBill,
+		CreatedAt:     data.CreatedAt,
+		OrderStatus:   data.OrderStatus,
+		ProductStatus: data.ProductStatus,
+		InvoiceNumber: data.InvoiceNumber,
+		InvoiceUrl:    data.InvoiceUrl,
+		PaymentUrl:    data.PaymentUrl,
+	}
+}
