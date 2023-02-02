5995c047ed30b822ec8e5801792a2154d508dc05
Muhamad Fauzan Putra <business.mfauzanptra@gmail.com>
2023-02-02 13:09:27 +0700

    perf: add validation for add product

--- features/product/entity.go
+++ features/product/entity.go
@@ -10,13 +10,13 @@ type Core struct {
 	ID           uint    `json:"id" form:"id"`
 	UserId       uint    `json:"user_id" form:"user_id"`
 	UserName     string  `json:"user_name" form:"user_name"`
-	Upc          string  `json:"upc" form:"upc"`
-	Category     string  `json:"category" form:"category"`
-	ProductName  string  `json:"product_name" form:"product_name"`
-	Stock        int     `json:"stock" form:"stock"`
-	MinimumStock int     `json:"minimum_stock" form:"minimum_stock"`
-	BuyingPrice  float64 `json:"buying_price" form:"buying_price"`
-	Price        float64 `json:"price" form:"price"`
+	Upc          string  `json:"upc" form:"upc" validate:"required"`
+	Category     string  `json:"category" form:"category" validate:"required"`
+	ProductName  string  `json:"product_name" form:"product_name" validate:"required"`
+	Stock        int     `json:"stock" form:"stock" validate:"required,numeric"`
+	MinimumStock int     `json:"minimum_stock" form:"minimum_stock" validate:"numeric"`
+	BuyingPrice  float64 `json:"buying_price" form:"buying_price" validate:"numeric"`
+	Price        float64 `json:"price" form:"price" validate:"required,numeric"`
 	ProductImage string  `json:"product_image" form:"product_image"`
 	Supplier     string  `json:"supplier" form:"supplier"`
 	ItemsSold    int     `json:"items_sold" form:"items_sold"`
