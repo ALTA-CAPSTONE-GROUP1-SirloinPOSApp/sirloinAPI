37ee675da8c1d00ea79e85167a34b7cf73303bc0
amrizal94 <arimrizal94@gmail.com>
2023-02-04 11:56:46 +0700

    test: unit test feature suctomer updata data (#58)

--- features/customer/services/service_test.go
+++ features/customer/services/service_test.go
@@ -148,3 +148,147 @@ func TestAdd(t *testing.T) {
 	})
 
 }
+
+func TestUpdate(t *testing.T) {
+
+	data := mocks.NewCustomerData(t)
+	newCustomer := customer.Core{
+		Email:       "mfauzanptra@gmail.com",
+		Name:        "Muhamad Fauzan Putra",
+		PhoneNumber: "085659171799",
+		Address:     "Jln. Lembayung No 24, Bantul, Yogyakarta",
+	}
+
+	expectedData := customer.Core{
+		Email:       "mfauzanptra@gmail.com",
+		Name:        "Muhamad Fauzan Putra",
+		PhoneNumber: "085659171799",
+		Address:     "Jln. Lembayung No 24, Bantul, Yogyakarta",
+	}
+
+	userId := 2
+	customerId := 1
+
+	t.Run("success edit customer data", func(t *testing.T) {
+		data.On("Update", uint(userId), uint(customerId), newCustomer).Return(expectedData, nil).Once()
+		srv := New(data)
+		_, token := helper.GenerateJWT(userId)
+		pToken := token.(*jwt.Token)
+		pToken.Valid = true
+		res, err := srv.Update(pToken, uint(customerId), newCustomer)
+		assert.Nil(t, err)
+		assert.Equal(t, expectedData.ID, res.ID)
+		assert.Equal(t, expectedData.Name, res.Name)
+		data.AssertExpectations(t)
+	})
+
+	t.Run("server problem", func(t *testing.T) {
+		data.On("Update", uint(userId), uint(customerId), newCustomer).Return(customer.Core{}, errors.New("server problem")).Once()
+		srv := New(data)
+
+		_, token := helper.GenerateJWT(userId)
+		pToken := token.(*jwt.Token)
+		pToken.Valid = true
+
+		res, err := srv.Update(pToken, uint(customerId), newCustomer)
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "server")
+		assert.Equal(t, res.Name, "")
+		data.AssertExpectations(t)
+	})
+
+	t.Run("data not found", func(t *testing.T) {
+		data.On("Update", uint(userId), uint(customerId), newCustomer).Return(customer.Core{}, errors.New("data not found")).Once()
+
+		srv := New(data)
+
+		_, token := helper.GenerateJWT(userId)
+		pToken := token.(*jwt.Token)
+		pToken.Valid = true
+
+		res, err := srv.Update(pToken, uint(customerId), newCustomer)
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "not found")
+		assert.Equal(t, res.Name, "")
+		data.AssertExpectations(t)
+	})
+
+	t.Run("jwt not valid", func(t *testing.T) {
+		srv := New(data)
+		_, token := helper.GenerateJWT(1)
+		pToken := token.(*jwt.Token)
+		pToken.Valid = true
+		res, err := srv.Update(pToken, uint(customerId), newCustomer)
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "token error")
+		assert.Equal(t, uint(0), res.ID)
+	})
+
+	t.Run("email already exist", func(t *testing.T) {
+		data.On("Update", uint(userId), uint(customerId), newCustomer).Return(customer.Core{}, errors.New("Duplicate customers.email")).Once()
+		srv := New(data)
+
+		_, token := helper.GenerateJWT(2)
+		pToken := token.(*jwt.Token)
+		pToken.Valid = true
+		res, err := srv.Update(pToken, uint(customerId), newCustomer)
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "user already exist")
+		assert.Equal(t, res.Name, "")
+		data.AssertExpectations(t)
+	})
+
+	t.Run("phone number already exist", func(t *testing.T) {
+		data.On("Update", uint(userId), uint(customerId), newCustomer).Return(customer.Core{}, errors.New("Duplicate customers.phone_number")).Once()
+		srv := New(data)
+
+		_, token := helper.GenerateJWT(2)
+		pToken := token.(*jwt.Token)
+		pToken.Valid = true
+		res, err := srv.Update(pToken, uint(customerId), newCustomer)
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "phone number already exist")
+		assert.Equal(t, res.Name, "")
+		data.AssertExpectations(t)
+	})
+
+	t.Run("Name validasi", func(t *testing.T) {
+		srv := New(data)
+
+		_, token := helper.GenerateJWT(2)
+		pToken := token.(*jwt.Token)
+		pToken.Valid = true
+		newCustomer.Name = "AMR#%^%"
+		res, err := srv.Update(pToken, uint(customerId), newCustomer)
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "alpha_space")
+		assert.Equal(t, res.Name, "")
+	})
+
+	t.Run("Email validasi", func(t *testing.T) {
+		srv := New(data)
+		_, token := helper.GenerateJWT(2)
+		pToken := token.(*jwt.Token)
+		pToken.Valid = true
+		newCustomer.Name = "Toko Ira"
+		newCustomer.Email = "tokoira.com"
+		res, err := srv.Update(pToken, uint(customerId), newCustomer)
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "email")
+		assert.Equal(t, res.Name, "")
+	})
+
+	t.Run("Phone number validasi", func(t *testing.T) {
+		srv := New(data)
+		_, token := helper.GenerateJWT(2)
+		pToken := token.(*jwt.Token)
+		pToken.Valid = true
+		newCustomer.Name = "Toko Ira"
+		newCustomer.Email = "tokoira@gmail.com"
+		newCustomer.PhoneNumber = "oiajsdojhasodi"
+		res, err := srv.Update(pToken, uint(customerId), newCustomer)
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "PhoneNumber")
+		assert.Equal(t, res.Name, "")
+	})
+}
