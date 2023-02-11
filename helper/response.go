ffad1ad058cbf3a2bb860babcd3eb35343f8b4e0
amrizal94 <arimrizal94@gmail.com>
2023-02-11 07:24:01 +0700

    fix: add response update bussiname name validation

--- helper/response.go
+++ helper/response.go
@@ -24,38 +24,52 @@ func PrintErrorResponse(msg string) (int, interface{}) {
 		log.Println("error running register service: user already exist")
 		resp["message"] = "email already exist"
 		code = http.StatusConflict
+
 	} else if strings.Contains(msg, "phone number already exist") {
 		log.Println("error running register service: phone number already exist")
 		code = http.StatusConflict
 		resp["message"] = "phone number already exist"
+
 	} else if strings.Contains(msg, "secure_password") {
 		log.Println("error running register service: the password does not meet security requirements")
 		code = http.StatusBadRequest
 		resp["message"] = "password must be at least 8 characters long, must contain uppercase letters, must contain lowercase letters, must contain numbers, must not be too general"
+
 	} else if strings.Contains(msg, "required") {
 		log.Println("error running register service: required fields")
 		code = http.StatusBadRequest
 		resp["message"] = "required fields must be filled"
+
 	} else if strings.Contains(msg, "PhoneNumber") && strings.Contains(msg, "numeric") {
 		log.Println("error running register service: phone number must be numeric")
 		code = http.StatusBadRequest
 		resp["message"] = "the phone number must be a number"
+
 	} else if strings.Contains(msg, "BusinessName") && strings.Contains(msg, "alpha_space") {
 		log.Println("error running register service: business names must be alpha_space")
 		code = http.StatusBadRequest
-		resp["message"] = "customer names are only allowed to contain letters and spaces"
+		resp["message"] = "business names are only allowed to contain letters and spaces"
+
+	} else if strings.Contains(msg, "update business name") && strings.Contains(msg, "alpha_space") {
+		log.Println("error running update user service: business names must be alpha_space")
+		code = http.StatusBadRequest
+		resp["message"] = "business names are only allowed to contain letters and spaces"
+
 	} else if strings.Contains(msg, "AddCustomerValidate.Name") && strings.Contains(msg, "alpha_space") {
 		log.Println("error running add customer service: business names must be alpha_space")
 		code = http.StatusBadRequest
 		resp["message"] = "customer names are only allowed to contain letters and spaces"
+
 	} else if strings.Contains(msg, "Email") && strings.Contains(msg, "email") {
 		log.Println("error running register service: Email must be email format")
 		code = http.StatusBadRequest
 		resp["message"] = "incorrect e-mail format"
+
 	} else if strings.Contains(msg, "token error") && strings.Contains(msg, "customer") {
 		log.Println("error running register service: extract token error")
 		code = http.StatusBadRequest
 		resp["message"] = "extract token error/not allowed to access customer"
+
 	} else {
 		log.Println("error running register service")
 		code = http.StatusInternalServerError
